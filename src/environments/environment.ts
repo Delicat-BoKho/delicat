8ba380bd10d8a5e34039a78b969368be2602cacd
chicophi <tranhphi9c1@gmail.com>
2023-04-24 22:55:07 +0700

    set Firebase and Configure Package.json to fit Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'delicatadmin',
+    appId: '1:390971250724:web:f8697666b4257d670aa0b1',
+    storageBucket: 'delicatadmin.appspot.com',
+    locationId: 'asia-southeast2',
+    apiKey: 'AIzaSyCkA1OOcBKxRsnsFJqFIRQhobtxiHzYqQA',
+    authDomain: 'delicatadmin.firebaseapp.com',
+    messagingSenderId: '390971250724',
+    measurementId: 'G-HNPBHJVEWL',
+  },
   production: false
 };
 
